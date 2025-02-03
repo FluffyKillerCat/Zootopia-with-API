511705c3b7d462ce57dfc9327ed3b2f97898fbe2
Joud Hajal <joudhajal@Mac.fritz.box>
2025-02-03 12:24:35 +0100

    Updated git ignore

--- data_fetcher.py
+++ data_fetcher.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
+
 def fetch_data(animal):
     URL = f"https://api.api-ninjas.com/v1/animals?name={animal}"
     res = requests.get(URL, headers={'X-Api-Key': API_KEY})
