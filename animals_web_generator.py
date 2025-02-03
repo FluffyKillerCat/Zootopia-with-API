b802594d96441f0700e1f9e79970dc4935bbcda8
Joud Hajal <joudhajal@Mac.fritz.box>
2025-02-03 12:18:52 +0100

    Added donenv()

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,5 +1,5 @@
 import json
-import requests
+
 import data_fetcher
 
 
--- data_fetcher.py
+++ data_fetcher.py
@@ -1,3 +1,8 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
 def fetch_data(animal):
     URL = f"https://api.api-ninjas.com/v1/animals?name={animal}"
     res = requests.get(URL, headers={'X-Api-Key': API_KEY})
